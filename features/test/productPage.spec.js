cb2f32f4ae1b15c66a6c05028f91d00982fc8cc9
rodrigo.cabral <rodrigo.oliveiracabral@hotmail.com>
2024-11-03 20:05:35 +0000

    Refactor HomePage methods and enhance product search functionality in tests

--- features/test/productPage.spec.js
+++ features/test/productPage.spec.js
@@ -1,7 +1,25 @@
 import { test, expect } from '@playwright/test';
+import HomePage from '../page/homePage.page';
+
+const homePage = new HomePage();
+const product = 'Printed Dress';
+let productsTarget;
+
+test.beforeEach(async ({ browser }) => {
+    global.context = await browser.newContext();
+    global.page = await context.newPage();
+    await homePage.navigateHomePage();
+    productsTarget = await homePage.SearchProduct(product);
+});
 
 
-test('should open the product page', async ({ page }) => {})
+test('should open the product page', async ({  }) => {
+    await homePage.clickProduct(productsTarget);
+    
 
-test('should add product to cart', async ({ page }) => {})
+})
+
+test('should add product to cart', async ({  }) => {
+
+})
 
