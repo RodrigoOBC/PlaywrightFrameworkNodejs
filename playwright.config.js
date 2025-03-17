15c8cd2fb96fa2c15129a2a2738fd5ba98d5825b
rodrigo.cabral <rodrigo.cabral@radixeng.com.br>
2025-03-17 12:14:36 -0300

    Enhance Playwright configuration with ortoni-report integration and additional browser support

--- playwright.config.js
+++ playwright.config.js
@@ -1,6 +1,21 @@
 // Developed with care by RodrigoOBC.
 import { defineConfig, devices } from '@playwright/test';
 
+
+const reportConfig = {
+  open: process.env.CI ? "never" : "always",
+  folderPath: ".temp/otoni-report",
+  filename: "index.html",
+  title: "Playwright Test Report",
+  showProject: !true,
+  projectName: "RodrigoBOCabral",
+  testType: "e2e",
+  authorName: "Koushik (LetCode with Koushik)",
+  base64Image: false,
+  stdIO: false,
+  preferredTheme: "dark"
+};
+
 export default defineConfig({
   // Look for test files in the "tests" directory, relative to this configuration file.
   testDir: './features/test',
@@ -15,10 +30,10 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
 
   // Opt out of parallel tests on CI.
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 1 : 3,
 
   // Reporter to use
-  reporter: 'html',
+  reporter: [['line'],["ortoni-report", reportConfig]],
 
   use: {
     // Base URL to use in actions like `await page.goto('/')`.
@@ -34,6 +49,10 @@ export default defineConfig({
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
     },
   ],
 });
